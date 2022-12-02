b55f95f398ce88dcbaab246d4e380c8163a88390
Musegh <118133145+MusheghMartirosyan@users.noreply.github.com>
2022-12-02 21:06:02 +0400

    added Flags-App

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,5 +1,6 @@
 import './Portfolio.css'
 import Image from '../../images/maxresdefault.jpg'
+import Flags from '../../images/Flags-App Screenshot.png'
 const Portfolio = () => {
     return(
         <section id='portfolio'>
@@ -8,17 +9,17 @@ const Portfolio = () => {
             <div className='container portfolio-container'>
                 <article className='portfolio-item'>
                     {/* <div className='portfolio-item-img'></div> */}
-                    <img src={Image} />
-                    <h3>Title</h3>
-                    <a className='btn' href='#'>GitHub Link</a>
-                    <a className='btn btn-primary' href='#'>Live Demo</a>
+                    <img src={Flags} />
+                    <h3>Flags-App</h3>
+                    <a className='btn' target='_blank' href='https://github.com/MusheghMartirosyan/Flags-App'>GitHub Link</a>
+                    <a className='btn btn-primary' target='_blank' href='https://musheghmartirosyan.github.io/Flags-App/'>Live Demo</a>
                 </article>
                 <article className='portfolio-item'>
                     {/* <div className='portfolio-item-img'></div> */}
                     <img src={Image} />
                     <h3>Title</h3>
-                    <a className='btn' href='#'>GitHub Link</a>
-                    <a className='btn btn-primary' href='#'>Live Demo</a>
+                    <a className='btn' target='_blank' href='#'>GitHub Link</a>
+                    <a className='btn btn-primary' target='_blank' href='#'>Live Demo</a>
                 </article>
                 <article className='portfolio-item'>
                     {/* <div className='portfolio-item-img'></div> */}
